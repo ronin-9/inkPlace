ad94bc886680643c1862cd5f2bfe25343ddec23e
cgomez65 <carlosgomez7361@gmail.com>
2019-04-20 10:38:51 -0500

    addedstyle

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,11 @@ import Select from 'react-select';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const techCompanies = [
-  { label: "Apple", value: 1 },
-  { label: "Facebook", value: 2 },
-  { label: "Netflix", value: 3 },
-  { label: "Tesla", value: 4 },
-  { label: "Amazon", value: 5 },
-  { label: "Alphabet", value: 6 },
+  { label: "Japanese", value: 1 },
+  { label: "BlackWork", value: 2 },
+  { label: "OldSchool", value: 3 },
+  { label: "Tribal", value: 4 },
+
 ];
 
 
