a07dfad470cd09db6621841d0f26c43b987a6a9a
YordaniBonilla <b.yordani93@gmail.com>
2019-04-20 13:27:41 -0500

    added routes and route components

--- client/src/Index.js
+++ client/src/Index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
-import Users from '/.users';
-import Contact from './contact';
+import Users from './Components/Users.jsx';
+import Contact from './Components/Contact.jsx';
 import { Route, Link, BrowserRouter as Router} from 'react-router-dom';
 
-render(<App />, document.getElementById('app'));
+
+
+const routing = (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+        <Route exact path="/" component={App} />
+        <Route path="/users" component={Users} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </Router>
+  )
+  
+render(routing, document.getElementById('root'));
