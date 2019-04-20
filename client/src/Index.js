80f181aeaba7062e1a1716c14b4175c401bec445
cgomez65 <carlosgomez7361@gmail.com>
2019-04-20 11:52:06 -0500

    added react-router-dom

--- client/src/Index.js
+++ client/src/Index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import Users from '/.users';
+import Contact from './contact';
+import { Route, Link, BrowserRouter as Router} from 'react-router-dom';
 
 render(<App />, document.getElementById('app'));
