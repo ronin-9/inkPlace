6399fde8ca3b368446b1fd64297ad28b90e9ce41
YordaniBonilla <b.yordani93@gmail.com>
2019-04-22 11:23:43 -0500

    load images succesful

--- client/src/Components/Images.jsx
+++ client/src/Components/Images.jsx
@@ -1,38 +1,38 @@
 import React from 'react';
 import Img from 'react-image';
-
+import Image from 'react-image-resizer';
+const style = {
+  image: {
+    border: '1px solid #ccc',
+    background: '#fefefe',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+};
 class Images extends React.Component {
     render() {
        return(
         <div> 
             <h1>BlackWork</h1> 
-        <img src={[
-      require('../blackwork/body.jpg'),
-      '../blackwork/forearm.jpg',
-      '../blackwork/legs.jpg',
-      '../blackwork/snake.jpg'
-    ]}></img> 
+        <img height={ 100 }
+          width={ 100} style={style.img} src={
+      require('../blackwork/body.jpg')
+    }></img>
+    
         <h1>OldSchool</h1>
-        <img src={[
+        <img height={ 100 }
+          width={ 100} style={style.img} src={[
       require('../oldSchool/one.jpg'),
-      '../oldSchool/two.jpg',
-      '../oldSchool/three.jpg',
-      '../oldSchool/four.jpg'
     ]}></img>
         <h1>Tribal</h1>
-        <img src={[
-      '../tribal/one.jpg',
-      '../tribal/two.jpg',
-      '../tribal/three.jpg',
-      '..tribal/four.jpg'
+        <img height={ 100 }
+          width={ 100} style={style.img} src={[
+      require('../tribal/one.jpg'),
     ]}></img>
         <h1>Japanese</h1>
-        <img src={[
-      '../japanese/back.jpg',
-      '../japanese/legs.jpg',
-      '../japanese/low.jpg',
-      '../japanese/orig.jpeg',
-      '../japanese/shoulder.jpg',
+        <img height={ 100 }
+          width={ 100} style={style.img} src={[
+      require('../japanese/back.jpg'),
     ]}></img>
         </div>
        );
